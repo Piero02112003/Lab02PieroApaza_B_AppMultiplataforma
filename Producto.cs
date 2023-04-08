9438bea1fd590f6ee33ee2cb3453abaae6dd980f
Piero_Apaza <piero02112003@gmail.com>
2023-04-08 01:18:16 -0500

    first commit

---
+++ Producto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product
+{
+    public class Producto
+    {
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public DateTime FechadeVencimiento { get; set; }
+        public string Descripcion { get; set; }
+
+        public Producto(string nombre, decimal precio, DateTime fechadeVencimiento, string descripcion)
+        {
+            Nombre = nombre;
+            Precio = precio;
+            FechadeVencimiento = fechadeVencimiento;
+            Descripcion = descripcion;
+        }
+
+        // El producto está vencido
+        public bool EstaVencido()
+        {
+            return DateTime.Now > FechadeVencimiento;
+        }
+
+        // Precio de venta con IGV
+        public decimal PrecioVenta()
+        {
+            decimal igv = Precio * 0.18m; 
+            return Precio + igv; 
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Product;
+
+namespace Lab02PieroApaza
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Producto producto = new Producto("Producto de prueba", 10.0m, new DateTime(2023, 12, 31), "Descripción de prueba");
+
+            // Información si el producto está vencido
+            if (producto.EstaVencido())
+            {
+                Console.WriteLine("El producto se ha pasado de su fecha, está vencido.");
+            }
+            else
+            {
+                Console.WriteLine("El producto sigue vigente, no está vencido.");
+            }
+
+            // Precio de venta del producto
+            decimal precioVenta = producto.PrecioVenta();
+            Console.WriteLine("El precio de venta del producto es: " + precioVenta);
+        }
+    }
+}
+
+   
+
